801f58c0ad190913d2c7409a570a02025ea4c222
Alexandro Disla <alexandrodisla@hotmail.com>
2020-09-11 16:57:25 -0400

    component reusabel button with slot

--- src/library/Library.svelte
+++ src/library/Library.svelte
@@ -1,11 +1,34 @@
 <script>
-    // your script goes here
+    import Button from "../common/Button.svelte";
 </script>
 
 <style>
+    header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
 
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
 </style>
 
-<div>
-    <h1> My Library </h1>
-</div>
+<header>
+  <span class="preamble">Welcome to the</span>
+  <h1>Library</h1>
+</header>
+
+<p class="greeting">
+  This is a library for the people. Welcome. Read the books here. Be inspired.
+  Go home, and share them with your family.
+</p>
+
+<Button>+ Add Text</Button>
